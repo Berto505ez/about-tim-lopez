88a7ea977e5e219321c215a3b3151a0938c0b21e
Alberto Lopez <alopez527@cnm.edu>
2022-01-12 11:32:21 -0600

    Commit and push about me project.

---
+++ psuedocode.js
@@ -0,0 +1,39 @@
+//forEach number between 1-20 execute loop.
+//while looping between 1-20 check every number.
+// if the number is even print fizz
+//if number is divisible by 5 output buzz
+//else output foo
+//
+//given a number i and a number j define i and j
+//loop between i and j assuming i is less than j
+//declare a value sum that is initialized at 0
+//if iterator is odd add iterator to the sum
+//else continue
+//return sum
+
+//for (let i= 1; i<= 20; i++){
+    //console.log("iterator", i)
+    //if (i % 2 === 0 || i % 5=== 0){
+        //if (i% 2=== 0){
+            //console.log("fizz")
+        //}
+        //if (i% 5=== 0){
+            //console.log("buzz")
+        //}
+    //} else{
+        //console.log("foo")
+    //}
+//}
+
+//forEach number between 1-20 execute loop.
+//while looping between 1-20 check every number.
+// if the number is even print fizz
+//if number is divisible by 5 output buzz
+//else output foo
+//
+//given a number i and a number j define i and j
+//loop between i and j assuming i is less than j
+//declare a value sum that is initialized at 0
+//if iterator is odd add iterator to the sum
+//else continue
+//return sum
